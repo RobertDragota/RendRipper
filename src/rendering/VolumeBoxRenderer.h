b43f6976fe79c9a5493bfe9b2a56498b39b8fb17
RobertDragota <121293266+RobertDragota@users.noreply.github.com>
2025-06-13 23:55:53 +0300

    Enhance volume box appearance

--- src/rendering/VolumeBoxRenderer.h
+++ src/rendering/VolumeBoxRenderer.h
@@ -10,9 +10,11 @@ public:
     ~VolumeBoxRenderer();
 
     void Init(float halfX, float halfY, float height);
+    void SetLineWidth(float width) { lineWidth_ = width; }
     void Render(const glm::mat4 &view, const glm::mat4 &proj, const glm::vec3 &color);
 
 private:
     GLuint vao_ = 0, vbo_ = 0;
     std::unique_ptr<Shader> shader_;
+    float lineWidth_ = 1.0f;
 };
